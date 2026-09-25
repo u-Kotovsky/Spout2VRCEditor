6444efdf0b7031f4686e9203b428bb3253025b6a
u-Kotovsky <id.u.kotovsky@gmail.com>
2026-09-25 22:24:09 +0300

    fix: put CustomEditorGUI into Spout2VRCEditor namespace

--- Packages/com.spout2vrceditor/Runtime/CustomEditorGUI.cs
+++ Packages/com.spout2vrceditor/Runtime/CustomEditorGUI.cs
@@ -1,50 +1,52 @@
 #if UNITY_EDITOR && !UDONSHARP_COMPILER
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class CustomEditorGUI
+namespace Spout2VRCEditor
 {
-    public Object targetObject;
-    public Editor targetEditor;
-    public bool allowFolding;
-
-    public CustomEditorGUI(Object obj, bool allowFolding = true)
+    public class CustomEditorGUI
     {
-        targetObject = obj;
-        
-        if (targetObject != null)
-        {
-            targetEditor = Editor.CreateEditor(targetObject);
-        }
-    }
+        public Object targetObject;
+        public Editor targetEditor;
+        public bool allowFolding;
 
-    public bool IsFolded { get; set; }
-
-    public void OnInspectorGUI()
-    {
-        if (targetObject == null)
+        public CustomEditorGUI(Object obj, bool allowFolding = true)
         {
-            EditorGUILayout.LabelField("No target selected");
-            return;
+            targetObject = obj;
+
+            if (targetObject != null)
+            {
+                targetEditor = Editor.CreateEditor(targetObject);
+            }
         }
 
-        // No editor created, though it should never happen
-        //if (targetEditor == null) return;
+        public bool IsFolded { get; set; }
 
-        if (allowFolding)
+        public void OnInspectorGUI()
         {
-            IsFolded = EditorGUILayout.BeginFoldoutHeaderGroup(IsFolded, $"{targetObject.name}");
-            if (!IsFolded)
+            if (targetObject == null)
+            {
+                EditorGUILayout.LabelField("No target selected");
+                return;
+            }
+
+            // No editor created, though it should never happen
+            //if (targetEditor == null) return;
+
+            if (allowFolding)
+            {
+                IsFolded = EditorGUILayout.BeginFoldoutHeaderGroup(IsFolded, $"{targetObject.name}");
+                if (!IsFolded)
+                {
+                    targetEditor.OnInspectorGUI();
+                }
+
+                EditorGUILayout.EndFoldoutHeaderGroup();
+            }
+            else
             {
                 targetEditor.OnInspectorGUI();
             }
-            EditorGUILayout.EndFoldoutHeaderGroup();
-        }
-        else
-        {
-            targetEditor.OnInspectorGUI();
         }
     }
 }
